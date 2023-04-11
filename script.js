efd141ceaf02f872562084d710b746b40b3f0e59
zielak146 <125725768+zielak146@users.noreply.github.com>
2023-04-11 09:09:34 +0100

    JS
    JS update

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-const toggleButton = document.getElementsByClassName('toggle-btn')[0]
-const navbar = document.getElementsByClassName('nav-menu')[0]
+const toggler = document.querySelector('.toggler');
+const menu = document.querySelector('.menu');
+const overlay = document.querySelector('.overlay');
+const drop = document.querySelector('.drop');
 
-toggleButton.addEventListener('click', () => {
-    navbar.classList.toggle('active')
-})
+toggler.addEventListener('change', function() {
+  if (this.checked) {
+    menu.style.visibility = 'visible';
+    overlay.style.transform = 'scale(1)';
+    overlay.style.transition = 'all 0.4s ease';
+    drop.style.opacity = 1;
+    drop.style.transition = 'opacity 0.4s ease';
+  } else {
+    menu.style.visibility = 'hidden';
+    overlay.style.transform = 'scale(0)';
+    overlay.style.transition = 'all 0.3s ease';
+    drop.style.opacity = 0;
+    drop.style.transition = 'opacity 0.4s ease';
+  }
+});
